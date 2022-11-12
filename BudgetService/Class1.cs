4337d09c5d00c4ecd15ef0b7f6e466a5fb34f2a6
Jerry <hsiangyu@cathaysec.com.tw>
2022-11-12 15:59:05 +0800

    feat: budget service init

---
+++ BudgetService/Class1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BudgetService
+{
+    public class BudgetService
+    {
+        private readonly IBudgetRepo _bugBudgetRepo;
+
+        public BudgetService(IBudgetRepo bugBudgetRepo)
+        {
+            this._bugBudgetRepo = bugBudgetRepo;
+        }
+
+        public decimal uery(DateTime start, DateTime end)
+        {
+            // 結束時間大於開始時間
+            if (end > start)
+            {
+                return 0;
+            }
+
+            var budgets = this._bugBudgetRepo.GetAll();
+            var startTYarMonth = Convert.ToInt32($"{start:yyyyMM}");
+            var endYearMonth = Convert.ToInt32($"{end:yyyyMM}");
+            var totalBudget=  budgets.Where(
+                rows => Convert.ToInt32(rows.YearMoth) >= startTYarMonth &&
+                        Convert.ToInt32(rows.YearMoth) <= endYearMonth);
+            var day = end.Subtract(start).Days;
+
+
+            // 當日查詢
+            if (day.Equals(1))
+            {
+                var monthDay = DateTime.DaysInMonth(start.Year, start.Month);
+
+                var data = totalBudget.Select(x => x.Amount).FirstOrDefault() / monthDay;
+            }
+
+
+
+            // 當月查詢
+
+            // 跨月查詢
+
+            // 部分月查詢
+        }
+    }
+
+    public interface IBudgetRepo
+    {
+        List<Budget> GetAll();
+    }
+
+    public class Budget
+    {
+        public string YearMoth { get; set; }
+
+        public int Amount { get; set; }
+    }
+}
